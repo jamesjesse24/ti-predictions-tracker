1426eab5c2fe9639f4656f6f63e200bf9de5c4f6
james jesse <G00335089@stu.raritanval.edu>
2026-08-01 05:30:37 +0800

    Update widget test for redesigned dashboard

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ti_predictions_tracker/main.dart';
 
 void main() {
-  testWidgets('shows automatic results dashboard', (tester) async {
+  testWidgets('shows redesigned TI command center', (tester) async {
     final controller = TrackerController.memory();
 
     await tester.pumpWidget(
@@ -18,8 +18,9 @@ void main() {
     );
     await tester.pumpAndSettle();
 
-    expect(find.text('Automatic feed: offline'), findsOneWidget);
+    expect(find.text('THE INTERNATIONAL\nPREDICTION COMMAND'), findsOneWidget);
     expect(find.text('Prediction performance'), findsOneWidget);
     expect(find.text('Latest completed series'), findsOneWidget);
+    expect(find.text('Enable result alerts'), findsOneWidget);
   });
 }
